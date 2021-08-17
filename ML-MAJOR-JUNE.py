28c585c5ac6654343ef146af0d950326eda5f551
Atharva Jadhao <67355863+Atharvajadhao@users.noreply.github.com>
2021-08-17 21:30:37 +0530

    Update ML-MAJOR-JUNE.py

--- ML-MAJOR-JUNE.py
+++ ML-MAJOR-JUNE.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pickle
+import pandas as pd
+import numpy as np
 
 Model = open('ML-MAJOR-JUNE_Model_Pkl','rb')
 classifier = pickle.load(Model)
@@ -8,8 +10,20 @@ st.title("Diabetes Predictor App")
 st.write('''
 This app predicts that a person is have Diabetes or not.
 ''')
+st.write('''
+Please enter the require information in given text box.
+''')
+st.write('''
+And please do not leave any text box blank.
+''')
+st.write('''
+Note:- The Machine Learning model is trained by diabetes.csv data set and the model has the accuracy of 82%.
+''')
+st.write('''
+Click here to access data set [link](https://github.com/Atharvajadhao/ML-MAJOR-JUNE/blob/main/data/diabetes.csv).
+''')
 
-st.sidebar.header("User Input Features")
+st.header("User Input Features")
 
 def user_input_feature():
     Pregnancies = st.text_input("Number of Pregnencies")
@@ -22,7 +36,7 @@ def user_input_feature():
     Age = st.text_input("Age")
     features = pd.DataFrame(data, index=[0])
     return features
-input_df = user_input_features()
+input_df = user_input_feature()
 
 if st.button("Predict"):
     result = classifier.predict(input_df)
